ddc883cdd9d8bfb82b640fef7545a3a1d81d590d
santiago alfredo bruno <tiagoabruno@hotmail.com>
2022-06-07 09:47:08 -0300

    se agrego tal cosa

--- Delivery.WEB/FRM_Paieses.aspx.vb
+++ Delivery.WEB/FRM_Paieses.aspx.vb
@@ -4,7 +4,7 @@ Public Class FRM_Paieses
     Inherits System.Web.UI.Page
     Protected Sub btn_BuscarImagen_Click(sender As Object, e As EventArgs) Handles btn_BuscarImagen.Click
 
-
+        lbl_Mensaje.Text = "Hola mundo"
 
     End Sub
 End Class
