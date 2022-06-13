1f5c8acc19c1b46f9ee688639c70fe9e4257af48
Gabrielvdr <gabrieldehesa43@gmail.com>
2022-06-13 11:44:32 -0300

    Registro Funciona / falta ocultar password

---
+++ Delivery.WEB/FRM_Registro.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="FRM_Registro.aspx.vb" Inherits="Delivery.WEB.FRM_Registro" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+
+        .auto-style1 {
+        }
+    </style>
+</head>
+<body>
+    <form id="FRM_Registro" runat="server">   
+        <table style="width: 100%;">
+            <tr>
+                <td class="auto-style1">
+                        <asp:ImageButton ID="btn_Logo" runat="server" Height="200px" ImageAlign="Middle" ImageUrl="~/Imagenes/Logo.png" Width="250px" />
+                </td>
+                <td>
+                    &nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style1" colspan="3">
+                    <hr />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">Nombre</td>
+                <td>
+                    <asp:TextBox ID="txtNombre" runat="server"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style1">Usuario</td>
+                <td>
+                    <asp:TextBox ID="txtUsuario" runat="server"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style1">Contraseña</td>
+                <td>
+                    <asp:TextBox ID="txtContraseña" runat="server" TextMode="Password"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style1">Confirmar contraseña</td>
+                <td>
+                    <asp:TextBox ID="txtConfirmarcontraseña" runat="server" TextMode="Password"></asp:TextBox>
+                    <asp:CheckBox ID="ChbMostrar" runat="server" Text="mostrar Contraseña" />
+                    <asp:ImageButton ID="ImageButton1" runat="server" />
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style1">&nbsp;</td>
+                <td id="lblAviso">
+                    <asp:Label ID="lbl_Mensaje" runat="server"></asp:Label>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style1">&nbsp;</td>
+                <td>
+                    <asp:Button ID="btnRegistro" runat="server" Text="Registar" OnClick="btnRegistro_Click" />
+                    <asp:Button ID="btnCancelar" runat="server" Text="Cancelar" />
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+    </form>
+ 
+  
+</body>
+</html>
---
+++ Delivery.WEB/FRM_Registro.aspx.designer.vb
@@ -0,0 +1,114 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class FRM_Registro
+
+    '''<summary>
+    '''Control FRM_Registro.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents FRM_Registro As Global.System.Web.UI.HtmlControls.HtmlForm
+
+    '''<summary>
+    '''Control btn_Logo.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btn_Logo As Global.System.Web.UI.WebControls.ImageButton
+
+    '''<summary>
+    '''Control txtNombre.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtNombre As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control txtUsuario.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtUsuario As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control txtContraseña.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtContraseña As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control txtConfirmarcontraseña.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtConfirmarcontraseña As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control ChbMostrar.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents ChbMostrar As Global.System.Web.UI.WebControls.CheckBox
+
+    '''<summary>
+    '''Control ImageButton1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents ImageButton1 As Global.System.Web.UI.WebControls.ImageButton
+
+    '''<summary>
+    '''Control lbl_Mensaje.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lbl_Mensaje As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control btnRegistro.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btnRegistro As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Control btnCancelar.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btnCancelar As Global.System.Web.UI.WebControls.Button
+End Class
